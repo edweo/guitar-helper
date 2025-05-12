d1d359df71785c9dba1011157243f8d159989186
edweo <edvinas.andrijaitis@proton.me>
2025-05-12 13:47:08 +0200

    added top bar and nav menu functionality

--- web/angular-guitar-helper/src/app/app.component.ts
+++ web/angular-guitar-helper/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {MatDrawer, MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatIconButton, MatDrawer, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-guitar-helper';
+  title = 'Guitara';
+  navOpened: boolean = false;
+
+  toggleNavMenu() {
+    this.navOpened = !this.navOpened;
+  }
 }
