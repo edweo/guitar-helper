d63b86f85d7e4848eed58b6dcd999c207092c82d
edweo <edvinas.andrijaitis@proton.me>
2025-05-23 16:05:41 +0200

    added sidenav for settings in chords practicing screen

--- web/angular-guitar-helper/src/app/pages/chords-page/nested-pages/chords-practice-start/chords-practice-start.component.ts
+++ web/angular-guitar-helper/src/app/pages/chords-page/nested-pages/chords-practice-start/chords-practice-start.component.ts
@@ -1,10 +1,16 @@
 import {Component, inject, OnDestroy} from '@angular/core';
 import {TopBarService} from '../../../../services/top-bar-service/top-bar.service';
 import {PageBackButtonComponent} from '../../../../components/page-back-button/page-back-button.component';
+import {TextIconButtonComponent} from '../../../../components/text-icon-button/text-icon-button.component';
+import {ComponentType} from '@angular/cdk/portal';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MobileModeService} from '../../../../services/mobile-mode-service/mobile-mode.service';
 
 @Component({
   selector: 'app-chords-practice-start',
-  imports: [],
+  imports: [
+    MatSidenavModule
+  ],
   templateUrl: './chords-practice-start.component.html',
   styleUrl: './chords-practice-start.component.css'
 })
@@ -12,6 +18,7 @@ export class ChordsPracticeStartComponent implements OnDestroy {
 
   // Services
   readonly topBarService = inject(TopBarService)
+  readonly mobileModeService = inject(MobileModeService)
 
   constructor() {
     this.topBarService.showTopBar()
@@ -19,6 +26,24 @@ export class ChordsPracticeStartComponent implements OnDestroy {
     this.topBarService.setLeftContent([
       {component: PageBackButtonComponent}
     ])
+
+    // TODO add counter maybe topbar right timer and/or toggle opne/closed menu settings
+
+    // this.topBarService.setRightContent([
+    //   {
+    //     component: TextIconButtonComponent,
+    //     init: (vcr, component: ComponentType<TextIconButtonComponent>) => {
+    //       const btn = vcr.createComponent(component)
+    //       btn.instance.matIcon = 'tune'
+    //       btn.instance.text = 'Configure'
+    //       btn.instance.onClick = this.placeholder
+    //     }
+    //   }
+    // ])
+  }
+
+  placeholder = () => {
+
   }
 
   ngOnDestroy(): void {
